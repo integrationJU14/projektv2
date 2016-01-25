084df74613ac82050ada8cc0ddbe90e6e7902d8c
Elena <Asus@192.168.158.40>
2016-01-25 23:11:56 +0100

    har lagt till 2 metoder i IssueAdapter

--- api/src/main/java/se/arole/api/adapter/IssueAdapter.java
+++ api/src/main/java/se/arole/api/adapter/IssueAdapter.java
@@ -1,28 +1,47 @@
 package se.arole.api.adapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import se.arole.api.resource.IssueVO;
 import se.arole.datalayer.entity.Issue;
 
 public class IssueAdapter {
 
 	public static Issue toIssueDb(IssueVO issue) {
-		
+
 		Integer issueId = issue.getIssueId();
 		String issueDescription = issue.getDescription();
 		String issueHeader = issue.getHeader();
 		boolean isSolved = issue.isSolved();
 
-		return new Issue(issueId, issueDescription, issueHeader, isSolved);
+		return new Issue(issueId, issueDescription, isSolved);
 	}
 
 	public static IssueVO fromIssueDb(Issue issueDb) {
-		
+
 		Integer issueId = issueDb.getIssueId();
-		String issueDescription = issueDb.getDescription();
-		String issueHeader = issueDb.getHeader();
+		String issueDescription = issueDb.getMessage();
+		String issueHeader = null;
 		boolean isSolved = issueDb.isSolved();
 
 		return new IssueVO(issueId, issueDescription, issueHeader, isSolved);
+	}
+
+	public static List<Issue> toDbIssueList(List<IssueVO> issueVOList) {
+		List<Issue> issueListToDb = new ArrayList<Issue>();
+		issueVOList.forEach(u -> {
+			issueListToDb.add(toIssueDb(u));
+		});
+		return issueListToDb;
+	}
+
+	public static List<IssueVO> fromDbIssueList(List<Issue> issueList) {
+		List<IssueVO> issueListFromDb = new ArrayList<IssueVO>();
+		issueList.forEach(u -> {
+			issueListFromDb.add(fromIssueDb(u));
+		});
+		return issueListFromDb;
 	}
 }
 //
