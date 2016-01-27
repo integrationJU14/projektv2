0edaec1ce5c7554f649ddfb1ce60d4691e25f2b7
Elena <Asus@192.168.158.40>
2016-01-28 00:05:17 +0100

    fixat lite Loader /Elena

--- web-api/src/main/java/se/arole/webapi/Loader.java
+++ web-api/src/main/java/se/arole/webapi/Loader.java
@@ -3,16 +3,6 @@ package se.arole.webapi;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-import org.glassfish.jersey.server.ResourceConfig;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-
-import se.arole.api.adapter.UserAdapter;
-import se.arole.api.controller.UserController;
-import se.arole.webapi.adapter.UserMapper;
-import se.arole.webapi.config.Config;
-import se.arole.webapi.resources.UserResource;
-
 @ApplicationPath("/*")
 public class Loader extends Application{
 
