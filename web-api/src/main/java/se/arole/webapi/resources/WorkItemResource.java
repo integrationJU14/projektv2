794463e41b7611818572f2f60ed5557d866fac0f
hejenstedt <l.hejenstedt@gmail.com>
2016-01-27 11:35:31 +0100

    POST i workitem

--- web-api/src/main/java/se/arole/webapi/resources/WorkItemResource.java
+++ web-api/src/main/java/se/arole/webapi/resources/WorkItemResource.java
@@ -1,11 +1,14 @@
 package se.arole.webapi.resources;
 
+import java.net.URI;
 import java.util.Collection;
 import java.util.List;
 
+import javax.json.JsonObjectBuilder;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
+import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
@@ -21,14 +24,21 @@ import javax.ws.rs.core.UriInfo;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
 import se.arole.api.controller.WorkItemController;
 import se.arole.api.resource.Team;
 import se.arole.api.resource.UserVO;
 import se.arole.api.resource.WorkItem;
+import se.arole.webapi.adapter.JsonObjectMapper;
 import se.arole.webapi.config.Config;
 
 @Path("work")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
 public class WorkItemResource {
+
 	private WorkItemController workItemController;
 
 	static {
@@ -48,77 +58,87 @@ public class WorkItemResource {
 	}
 
 	@GET
-	@Produces(MediaType.APPLICATION_JSON)
 	public Response getAllWorkItems() {
-
 		Collection<WorkItem> all = workItemController.getAll();
 		GenericEntity<Collection<WorkItem>> result = new GenericEntity<Collection<WorkItem>>(all) {
 		};
 		return Response.ok(result).build();
 	}
 
-//	@GET
-//	@Path("{id}")
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Response getUser(@PathParam("id") Integer id) {
-//
-//		WorkItem work = workItemController.findByItemId(id);
-//
-//		return Response.ok(work).build();
+	@POST
+	public Response addWorkItem(WorkItem workItem) {
+		WorkItem createWorkItem = workItemController.createWorkItem(workItem);
+		URI location = uriInfo.getAbsolutePathBuilder().path(createWorkItem.getWorkItemId() + "").build();
+		return Response.created(location).build();
+	}
 
-//	}
+	// @GET
+	// @Path("{id}")
+	// @Produces(MediaType.APPLICATION_JSON)
+	// public Response getUser(@PathParam("id") Integer id) {
+	//
+	// WorkItem work = workItemController.findByItemId(id);
+	//
+	// return Response.ok(work).build();
 
-//	@PUT
-//	@Path("{id}")
-//	@Consumes(MediaType.TEXT_PLAIN)
-//	@Produces(MediaType.TEXT_PLAIN)
-//	public Response changeStatusWorkItem(@QueryParam("status") @DefaultValue("TO_DO") String status,
-//			@PathParam("id") Integer id) {
-//		workItemController.changeStatusWorkItem(status, id);
-//
-//		return Response.status(Status.ACCEPTED).header("status changed  ", "work/" + id).build();
-//	}
-//
-//	@PUT
-//	@Path("{id}")
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	@Produces(MediaType.TEXT_PLAIN)
-//	public Response addWorkItemToUser(@PathParam("id") int workId, UserVO userVO) {
-//		WorkItem workItem = workItemController.findByItemId(workId);
-//		workItemController.addWorkItemToUser( workItem, userVO);
-//
-//		return Response.status(Status.ACCEPTED).header("workitem is updated", "work/" + workId)
-//				.build();
-//	}
-//
-//	@GET
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Response workItemByUser(UserVO userVO) {
-//		
-//		List<WorkItem> result = workItemController.workItembyUser(userVO);
-//
-//		return Response.ok(result).build();
-//	}
-//
-//	@GET
-//	@Consumes(MediaType.APPLICATION_JSON)
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Response workItemsByTeam(Team team) {
-//		List<WorkItem> result = workItemController.workItemsByTeam(team);
-//
-//		return Response.ok(result).build();
-//	}
-//
-//	@GET
-//	@Consumes(MediaType.TEXT_PLAIN)
-//	@Produces(MediaType.APPLICATION_JSON)
-//	public Response getWorkItemByStatus(@QueryParam("status") @DefaultValue("TO_DO") String status) {
-//		List<WorkItem> result = workItemController.workItemByStatus(status);
-//
-//		return Response.ok(result).build();
-//	}
-//
+	// }
+
+	// @PUT
+	// @Path("{id}")
+	// @Consumes(MediaType.TEXT_PLAIN)
+	// @Produces(MediaType.TEXT_PLAIN)
+	// public Response changeStatusWorkItem(@QueryParam("status")
+	// @DefaultValue("TO_DO") String status,
+	// @PathParam("id") Integer id) {
+	// workItemController.changeStatusWorkItem(status, id);
+	//
+	// return Response.status(Status.ACCEPTED).header("status changed ", "work/"
+	// + id).build();
+	// }
+	//
+	// @PUT
+	// @Path("{id}")
+	// @Consumes(MediaType.APPLICATION_JSON)
+	// @Produces(MediaType.TEXT_PLAIN)
+	// public Response addWorkItemToUser(@PathParam("id") int workId, UserVO
+	// userVO) {
+	// WorkItem workItem = workItemController.findByItemId(workId);
+	// workItemController.addWorkItemToUser( workItem, userVO);
+	//
+	// return Response.status(Status.ACCEPTED).header("workitem is updated",
+	// "work/" + workId)
+	// .build();
+	// }
+	//
+	// @GET
+	// @Consumes(MediaType.APPLICATION_JSON)
+	// @Produces(MediaType.APPLICATION_JSON)
+	// public Response workItemByUser(UserVO userVO) {
+	//
+	// List<WorkItem> result = workItemController.workItembyUser(userVO);
+	//
+	// return Response.ok(result).build();
+	// }
+	//
+	// @GET
+	// @Consumes(MediaType.APPLICATION_JSON)
+	// @Produces(MediaType.APPLICATION_JSON)
+	// public Response workItemsByTeam(Team team) {
+	// List<WorkItem> result = workItemController.workItemsByTeam(team);
+	//
+	// return Response.ok(result).build();
+	// }
+	//
+	// @GET
+	// @Consumes(MediaType.TEXT_PLAIN)
+	// @Produces(MediaType.APPLICATION_JSON)
+	// public Response getWorkItemByStatus(@QueryParam("status")
+	// @DefaultValue("TO_DO") String status) {
+	// List<WorkItem> result = workItemController.workItemByStatus(status);
+	//
+	// return Response.ok(result).build();
+	// }
+	//
 	@GET
 	@Consumes(MediaType.TEXT_PLAIN)
 	@Produces(MediaType.APPLICATION_JSON)
